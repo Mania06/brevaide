f07ab709c0ef41fcf41cc76c2aead3d0860cf64c
Mania06 <bossutsylvain@yahoo.fr>
2014-03-05 13:22:16 +0100

    Buttons du menus fonctionels

--- Brevaide/src/com/example/test/Brevaide.java
+++ Brevaide/src/com/example/test/Brevaide.java
@@ -2,7 +2,9 @@ package com.example.test;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class Brevaide extends Activity {
 
@@ -10,13 +12,50 @@ public class Brevaide extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_brevaide);
+		
+		/*
+		 * Buttons actions
+		 */
+		
+		
+		Button btQCM = (Button) findViewById(R.id.buttonQCM);
+		Button btTest = (Button) findViewById(R.id.buttonTest);
+		Button btStats = (Button) findViewById(R.id.buttonStats);
+		// QCM
+		
+		btQCM.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(Brevaide.this,QCM.class));
+			}
+
+		});
+		
+		// Test
+		
+		btTest.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(Brevaide.this,Test.class));
+			}
+
+		});
+
+		
+		// Stats
+		
+		btStats.setOnClickListener(new Button.OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				startActivity(new Intent(Brevaide.this,Stats.class));
+			}
+
+		});
+		
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.brevaide, menu);
-		return true;
-	}
 
 }
---
+++ Brevaide/src/com/example/test/Test.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Test extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+	}
+
+
+}
