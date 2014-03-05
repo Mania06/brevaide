da1b57a1bef7c8decd63c495a0441e549a92cb06
Mania06 <bossutsylvain@yahoo.fr>
2014-03-05 14:43:08 +0100

    look at bro !

--- Brevaide/src/com/example/test/QCM.java
+++ Brevaide/src/com/example/test/QCM.java
@@ -1,6 +1,8 @@
 package com.example.test;
 
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class QCM extends Activity {
@@ -9,6 +11,16 @@ public class QCM extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_qcm);
+		
+		TextView question = (TextView) findViewById(R.id.question);
+		RadioButton answer1 = (RadioButton) findViewById(R.id.answer1);
+		RadioButton answer2 = (RadioButton) findViewById(R.id.answer2);
+		RadioButton answer3 = (RadioButton) findViewById(R.id.answer3);
+		
+		question.setText("Bonsouère ! Es tu un battarre?");
+		answer1.setText("Oui");
+		answer2.setText("Non");
+		answer3.setText("Hahaha !!!!");
 	}
 
 }
